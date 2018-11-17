d8c33494d7e628341ce76e4fc4262c5aab71d1d5
Heroos <44765221+Heroos@users.noreply.github.com>
2018-11-18 00:30:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,21 +16,18 @@ client.on('message', message => {
       if (!devs1.includes(message.author.id)) return message.channel.send("<@175465689027444736> only this guy can do restart the bot so don't try again :wink:.");
       message.delete();
       client.user.setGame(argresult, 'https://twitch.tv/DynastyShop');
-	      message.channel.sendMessage(${argresult} stream changed to)
-          message.react(":white_check_mark:")
+      message.react(":white_check_mark:")
 
     } else if(message.content.startsWith(prefix + 'wt')) {
         client.user.setActivity(argresult,{type: 'WATCHING'});
-		message.channel.sendMessage(${argresult} Watching changed to)
-          message.react(":white_check_mark:")
+        message.react(":white_check_mark:")
 
       } else if(message.content.startsWith(prefix + 'setListening')) {
         client.user.setActivity(argresult,{type: 'LISTENING'});
 
       } else if(message.content.startsWith(prefix + 'pl')) {
         client.user.setActivity(argresult,{type: 'PLAYING'});
-		message.channel.sendMessage(${argresult} Playing changed to)
-          message.react(":white_check_mark:")
+        message.react(":white_check_mark:")
 
       } else if(message.content.startsWith(prefix + 'setName')) {
         client.user.setUsername(argresult);
