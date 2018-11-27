72c7cf3703929a231647ce1b701b57886ec5e43b
Heroos <44765221+Heroos@users.noreply.github.com>
2018-11-27 22:03:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = ".";
+const prefix = "^";
 const snekfetch = require("snekfetch");
 
 client.on('ready', () => {
